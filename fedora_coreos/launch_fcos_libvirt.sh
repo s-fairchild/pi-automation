ecaf5d373da829f9bde261564e29b6478e800e78
s-fairchild <steve51516@gmail.com>
2022-10-23 12:54:13 -0400

    Fix podman secrets passing to v4l2rtspserver, Add iso auto install creation script
    Podman secret env variables can't be used with command line arguments passed to container entrypoint.
    Created script and replaced entry point with bash -c that launches script. This way the secret vars can be used.

--- fedora_coreos/launch_fcos_libvirt.sh
+++ fedora_coreos/launch_fcos_libvirt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
-# IGNITION_CONFIG="$HOME/src/github/pi-automation/fedora_coreos/boss.ign"
+IGNITION_CONFIG="$HOME/src/github/pi-automation/fedora_coreos/kore_libvirt.ign"
 IGNITION_FILE="$1"
 IGNITION_CONFIG="/var/lib/libvirt/filesystems/$IGNITION_FILE"
-IMAGE="/var/lib/libvirt/images/pool/fedora-coreos-36.20220918.3.0-qemu.x86_64.qcow2"
+IMAGE="/var/lib/libvirt/images/pool/fedora-coreos-36.20221001.3.0-qemu.x86_64.qcow2"
+# IMAGE="/home/steven/.local/share/libvirt/images/fedora-coreos-36.20221001.3.0-qemu.x86_64.qcow2"
 VM_NAME="fcos-test-01"
 VCPUS="4"
 RAM_MB="4096"
@@ -13,18 +14,23 @@ RAID_DISK_GB="10"
 
 sudo cp "$IGNITION_FILE" "$IGNITION_CONFIG"
 sudo chcon --verbose --type svirt_home_t "$IGNITION_CONFIG"
-# chcon --verbose --type virt_image_t "$IMAGE"
+chcon --verbose --type virt_image_t "$IMAGE"
 sudo chown qemu: "$IGNITION_CONFIG"
-# sudo chown qemu: "$IMAGE"
+sudo chown qemu: "$IMAGE"
 
+# --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
 virt-install --connect="qemu:///system" --name="${VM_NAME}" --vcpus="${VCPUS}" --memory="${RAM_MB}" \
-        --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
-        --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
-        --disk="size=${DISK_GB},backing_store=${IMAGE}" \
-        --disk="size=${RAID_DISK_GB}" \
-        --disk="size=${RAID_DISK_GB}" \
-        --disk="size=${RAID_DISK_GB}" \
-        --disk="size=${RAID_DISK_GB}" \
-        --disk="size=${RAID_DISK_GB}" \
-        --network bridge=virbr0 \
-        --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}"
+    --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
+    --disk="size=${DISK_GB},backing_store=${IMAGE}" \
+    --disk="size=${RAID_DISK_GB}" \
+    --disk="size=${RAID_DISK_GB}" \
+    --disk="size=${RAID_DISK_GB}" \
+    --disk="size=${RAID_DISK_GB}" \
+    --disk="size=${RAID_DISK_GB}" \
+    --network network=default \
+    --boot menu=on,useserial=on \
+    --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}"
+
+# virsh attach-device "${VM_NAME}" --file cam_usb.xml --current
+
+sudo virsh destroy fcos-test-01 && sudo virsh undefine --remove-all-storage fcos-test-01
---
+++ fedora_coreos/scripts/create_custom_iso.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -x
+
+main() {
+
+    make production
+
+    ISOS="isos"
+    DEFAULT_ISO="fedora-coreos-36.20221001.3.0-live.x86_64.iso"
+    DEVICE="/dev/nvme0n1"
+    IGNITION="files/ignitions/kore_production.ign"
+    # IGNITION_URL="https://10.50.0.10:8000/kore_production.ign"
+    # POST_INSTALL_SCRIPT="files/bin/create_podman_secrets.sh"
+    NEW_ISO="${ISOS}/custom_$(date +%F)_${DEFAULT_ISO}"
+
+    check_remove_iso
+    create_new_iso
+
+}
+
+create_new_iso() {
+
+        # --post-install="${POST_INSTALL_SCRIPT}" \
+        # --ignition-url="${IGNITION_URL}" \
+    coreos-installer iso customize \
+        --dest-device="${DEVICE}" \
+        --dest-ignition "${IGNITION}" \
+        --force \
+        -o "${NEW_ISO}" "isos/${DEFAULT_ISO}"
+}
+
+check_remove_iso() {
+
+    if [[ -f $NEW_ISO ]]; then
+        echo "Deleting existing iso image ${NEW_ISO}"
+        if rm "${NEW_ISO}"; then
+            echo "Successfully deleted ${NEW_ISO}"
+        else
+            abort "Failed to delete ${NEW_ISO}"
+        fi
+    fi
+}
+
+
+# wipe_install_device() {
+
+    # if [[ -z $1 ]]; then
+        # abort "Installation device wasn't provided. Exiting."
+    # fi
+    # install_device="$1"
+
+    # read -n 1 -p "ALL DATA ON $install_device WILL BE REMOVED! Are you sure? [y/n]"
+    # if [[ $REPLY =~ [^y$] ]]
+
+# }
+
+abort() {
+
+    echo "$1"
+    exit 1
+}
+
+main
