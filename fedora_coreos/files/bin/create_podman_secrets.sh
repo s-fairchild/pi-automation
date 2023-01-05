2cbc97d2d1fb8219ee2bbda7fb108f51d52b8455
s-fairchild <steve51516@gmail.com>
2023-01-05 13:37:44 -0500

    Update podman secret creation, Secret file loading into ignition
    Secrets are now in a tar archive to allow for nameless loading of secrets.
    They will no longer have to be provided individually in butane config.

--- fedora_coreos/files/bin/create_podman_secrets.sh
+++ fedora_coreos/files/bin/create_podman_secrets.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-secret_dir=/var/container_secrets
-readarray -d '' secret_files < <(find "$secret_dir" -name "*.secret" -print0)
+main() {
+    secret_archive=/var/container_secrets
+    secrets="$(basename $secret_archive)"
+    tar xvf /var/container_secrets || abort "failed to extract ${secret_archive}"
+    readarray -d '' secret_files < <(find "$secrets" -name "*.secret" -print0)
 
-for s in ${secret_files[@]}; do
-    if [ -f "$s" ]; then
-        if podman secret create "$(basename $s)" "$s"; then
-            shred -zu "$s"
-        else
-            echo "$s not found, exiting..."
-            exit 1
+    for s in ${secret_files[@]}; do
+        if [ -f "$s" ]; then
+            if podman secret create "$(basename $s)" "$s"; then
+                shred -zu "$s"
+            else
+                abort "$s not found, exiting..."
+            fi
         fi
-    fi
-done
+    done
+
+    cleanup
+}
+
+cleanup() {
+    shred -zu "$secret_archive"
+    shred -zu "${secrets}/*" 
+    rm -rf "$secrets" 
+}
+
+#######################################
+# abort echos a message and exits with code 1
+#######################################
+abort() {
+    echo "${1}, Aborting."
+    exit 1
+}
+
+main
