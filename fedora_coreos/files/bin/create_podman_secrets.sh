6059f70d4b1c3f861b65720945dd85e1f69fdb47
s-fairchild <steve51516@gmail.com>
2023-01-08 10:13:35 -0500

    Update kore server to run motionplus
    Updating kore fedora coreos server to run motionplus rather than motion.
    Motionplus says some input sources can make use of audio, changing to record audio.

--- fedora_coreos/files/bin/create_podman_secrets.sh
+++ fedora_coreos/files/bin/create_podman_secrets.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 main() {
-    secret_archive=/var/container_secrets
-    secrets="$(basename $secret_archive)"
-    tar xvf /var/container_secrets || abort "failed to extract ${secret_archive}"
+    secret_archive=/var/container_secrets.tar
+    secrets="$(basename $secret_archive | cut -d . -f 1)"
+    tar xvf "$secret_archive" || abort "failed to extract ${secret_archive}"
     readarray -d '' secret_files < <(find "$secrets" -name "*.secret" -print0)
 
     for s in ${secret_files[@]}; do
         if [ -f "$s" ]; then
-            if podman secret create "$(basename $s)" "$s"; then
+            if podman secret create "$(basename $s | cut -d . -f 1)" "$s"; then
                 shred -zu "$s"
             else
                 abort "$s not found, exiting..."
@@ -20,9 +20,16 @@ main() {
 }
 
 cleanup() {
-    shred -zu "$secret_archive"
-    shred -zu "${secrets}/*" 
-    rm -rf "$secrets" 
+    if shred -zu "$secret_archive"; then
+        echo "Successfully shredded ${secret_archive}"
+    else
+        echo "failed to shred ${secret_archive}"
+    fi
+    if rm -rf "$secrets"; then
+        echo "Successfully deleted ${secrets}"
+    else
+        echo "failed to delete ${secrets}"
+    fi
 }
 
 #######################################
--- fedora_coreos/scripts/launch_fcos_libvirt.sh
+++ fedora_coreos/scripts/launch_fcos_libvirt.sh
@@ -1,35 +1,135 @@
-#!/bin/bash -x
+#!/bin/bash
 
-IGNITION_CONFIG="${HOME}/src/github/pi-automation/fedora_coreos/kore_libvirt.ign"
-IGNITION_FILE="${1:?Ignition file must be provided}"
-IGNITION_CONFIG="/var/lib/libvirt/filesystems/$(basename ${IGNITION_FILE})"
-IMAGE="${2:-/var/lib/libvirt/images/pool/fedora-coreos-37.20221106.3.0-qemu.x86_64.qcow2}"
-VM_NAME="fcos-test-01"
-VCPUS="4"
-RAM_MB="4096"
-STREAM="stable"
-DISK_GB="20"
-RAID_DISK_GB="10"
+set -o nounset -o errexit
 
-sudo cp "$IGNITION_FILE" "$IGNITION_CONFIG"
-sudo chcon --verbose --type svirt_home_t "$IGNITION_CONFIG"
-chcon --verbose --type virt_image_t "$IMAGE"
-sudo chown qemu: "$IGNITION_CONFIG"
-sudo chown qemu: "$IMAGE"
+# TODO add getopts to use flags
+main() {
+    def_path="/var/lib/libvirt/images/pool"
+    if [[ ${DOWNLOAD_IMAGE:-false} == "true" ]]; then
+        download_image "$img_dir"
+        echo "Copy downloaded image into ${def_path} and rerun ${0}"
+        exit 0
+    fi
+    echo "IGNITION_FILE location: ${IGNITION_FILE:?Ignition file must be provided. See ${0} -h for usage information.}"
+    IGNITION_CONFIG="/var/lib/libvirt/filesystems/$(basename ${IGNITION_FILE})"
+    latest_img="$(get_latest_image $def_path)"
+    IMAGE="${BACKING_STORAGE:-$latest_img}"
+    
+    # VM resources
+    VM_NAME="fcos-test-01"
+    VCPUS="4"
+    RAM_MB="4096"
+    STREAM="stable"
+    DISK_GB="20"
+    RAID_DISK_GB="10"
 
-# --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
-# --boot menu=on,useserial=on \
-virt-install --connect="qemu:///system" --name="${VM_NAME}" --vcpus="${VCPUS}" --memory="${RAM_MB}" \
-    --os-variant="fedora-coreos-$STREAM" \
-    --import \
-    --graphics=spice \
-    --disk="size=${DISK_GB},backing_store=${IMAGE}" \
-    --disk="size=${RAID_DISK_GB}" \
-    --disk="size=${RAID_DISK_GB}" \
-    --disk="size=${RAID_DISK_GB}" \
-    --disk="size=${RAID_DISK_GB}" \
-    --disk="size=${RAID_DISK_GB}" \
-    --network network=default \
-    --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}"
+    set_permissions
 
-# sudo virsh destroy fcos-test-01 && sudo virsh undefine --remove-all-storage fcos-test-01
+    # --tpm backend.type=emulator,backend.version=2.0,model=tpm-tis \
+    # --boot menu=on,useserial=on \
+    virt-install --connect="qemu:///system" --name="${VM_NAME}" --vcpus="${VCPUS}" --memory="${RAM_MB}" \
+        --os-variant="fedora-coreos-$STREAM" \
+        --import \
+        --graphics=spice \
+        --disk="size=${DISK_GB},backing_store=${IMAGE}" \
+        --disk="size=${RAID_DISK_GB}" \
+        --disk="size=${RAID_DISK_GB}" \
+        --disk="size=${RAID_DISK_GB}" \
+        --disk="size=${RAID_DISK_GB}" \
+        --disk="size=${RAID_DISK_GB}" \
+        --network network=default \
+        --qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}"
+
+    # sudo virsh destroy fcos-test-01 && sudo virsh undefine --remove-all-storage fcos-test-01
+}
+
+set_permissions() {
+    sudo cp "$IGNITION_FILE" "$IGNITION_CONFIG"
+    sudo chcon --verbose --type svirt_home_t "$IGNITION_CONFIG"
+    sudo chcon --verbose --type virt_image_t "$IMAGE"
+    sudo chown qemu: "$IGNITION_CONFIG"
+    sudo chown qemu: "$IMAGE"
+}
+
+get_latest_image() {
+    local p="$1"
+    local latest
+    latest="$(sudo ls -lhtr ${p} | tail -n -1 | cut -d ' ' -f 10)"
+    if [[ -z $latest ]]; then
+        abort "failed to find latest fedora coreos image"
+    fi
+    echo "${p}/${latest}"
+}
+
+is_root() {
+    if [[ ! $(id -u) -eq 0 ]]; then
+        abort "${0} must be ran as root or with sudo"
+    fi
+}
+
+download_image() {
+    local stream="stable"
+    img_dir="/var/lib/libvirt/images/pool"
+    local loc="${1:-$HOME/.local/share/libvirt/images/}"
+    podman run \
+            --pull=newer \
+            --name coreos-installer \
+            --rm \
+            -v "${loc}":/data \
+            -w /data \
+            quay.io/coreos/coreos-installer:release \
+                download -s "${stream}" \
+                -p qemu \
+                -f qcow2.xz \
+                --decompress
+}
+
+#######################################
+# abort echos a message and exits with code 1
+#######################################
+abort() {
+    echo "${1}, Aborting."
+    exit 1
+}
+
+usage() {
+    echo -e "USAGE:
+    ./${0} -f <IGNITION_FILE> [-d]
+
+ARGS:
+    -f          Ignition file for machine to use during bootstrap
+    -d          Download the latest ISO image
+    -h          This help message
+
+EXAMPLES:
+"
+
+    exit "${1:-0}"
+}
+
+while getopts ":f:db:" o; do
+    case "${o}" in
+        f)
+            IGNITION_FILE="${OPTARG}"
+            ;;
+        b)
+            # User provided image. If BACKING_STORAGE is null, the latest image is searched for.
+            BACKING_STORAGE="${OPTARG}"
+            ;;
+        d)
+            DOWNLOAD_IMAGE="true"
+            ;;
+        :)
+            abort "required option for ${OPTARG} not found"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [[ -z $* ]]; then
+    usage 1
+fi
+
+main "$@"
